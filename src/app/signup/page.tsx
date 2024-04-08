cec498c4509b4cdc2c6321ccd67124e90e3c1cd3
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-08 12:40:03 +0530

    signup form completed

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupForm from '../../components/auth/signup/SignupForm'
+
+function SignupPage() {
+  return (
+    <div className='w-100 bg-[#010409] min-h-screen max-h-fit flex justify-center items-center'>
+      <SignupForm/>
+    </div>
+  )
+}
+
+export default SignupPage
