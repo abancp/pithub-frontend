5c42ac4bc16e647ac630beb3d84ab18d73b5923d
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-08 12:39:49 +0530

    login form completed

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/src/components/auth/login/LoginForm'
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex justify-center items-center bg-[#0D1117] w-100 min-h-screen  max-h-fit'>
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default Login
