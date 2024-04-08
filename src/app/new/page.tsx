c7563f798685e5ad243c2c48a5a6fa20fc6da8a3
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-08 12:41:16 +0530

    auth completed

---
+++ src/app/new/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CreateRepo() {
+  return (
+    <div className='min-h-[calc(100vh-64px)] w-100 bg-[#010409] pt-11 flex justify-center items-start '>
+      <div className="w-5/6 lg:h-96 rounded-md flex text-white bg-[#0D1117]">
+        <div className="w-[50%]">
+            <h3 className='text-2xl font-bold'>Create new Repository</h3>
+        </div>
+        <div className="w-[50%]"></div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateRepo
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,10 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all js/jsx/ts/tsx files in your src directory
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
