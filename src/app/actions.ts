ffedc9b267ef8868eb6463798ee7de0369711ca6
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-12 20:03:52 +0530

    form validation using zod

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,15 @@
 'use server'
 import axios, { AxiosError, AxiosResponse } from "axios"
-import { ActionResponseState, LoginUser, SignupUser } from "../types/authTypes"
+import { ActionResponseState, LoginUser, SignupActionState, SignupUser } from "../types/authTypes"
+import { ZodError, z } from 'zod'
+
+const createUserFormSchema = z.object({
+    name: z.string().trim().min(3, "minimum 3 characters"),
+    username: z.string().trim().min(3, "minimum 3 characters"),
+    email: z.string().email("Invalid email"),
+    password: z.string().trim().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string()
+})
 
 export async function loginAction(currentState: ActionResponseState, formData: FormData): Promise<ActionResponseState> {
 
@@ -27,28 +36,86 @@ export async function loginAction(currentState: ActionResponseState, formData: F
     }
 }
 
-export async function signupAction(currentState: ActionResponseState, formData: FormData): Promise<ActionResponseState> {
-    
+export async function signupAction(currentState: SignupActionState, formData: FormData): Promise<SignupActionState> {
+
     const signupUser: SignupUser = {
         email: formData.get('email'),
         name: formData.get('name'),
         username: formData.get('username'),
-        password: formData.get('password')
+        password: formData.get('password'),
+        confirmPassword: formData.get('confirmPassword')
+    }
+
+    let returnState: SignupActionState = {
+        success: false,
+        errors: {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        message: ''
+    }
+
+    try {
+        createUserFormSchema.parse(signupUser)
+        returnState.success = true
+    } catch (e) {
+        if (e instanceof ZodError) {
+            for (const error of e.errors) {
+                let element: string = error.path[0] as string
+                switch (element) {
+                    case "name": {
+                        returnState.errors.name = error.message
+                        break
+                    }
+                    case "username": {
+                        returnState.errors.username = error.message
+                        break
+                    }
+                    case "email": {
+                        returnState.errors.email = error.message
+                        break
+                    }
+                    case "password": {
+                        returnState.errors.password = error.message
+                        break
+                    }
+                    case "confirmPassword": {
+                        returnState.errors.confirmPassword = error.message
+                        break
+                    }
+                }
+            }
+        }
+        returnState.message = "Something went wrong!"
+    }
+
+    if (!returnState.success) {
+        return returnState
     }
 
     try {
         const response: AxiosResponse = await axios.post("http://localhost:8000/signup", signupUser)
-        return response.data
+        return { success: response.data.success, message: response.data.message, errors: {} }
     } catch (e) {
         if (axios.isAxiosError(e)) {
             const axiosError: AxiosError = e as AxiosError
             if (axiosError.response) {
-                return { success: false, message: axiosError.response.data as string }
+                const responseErrMessage:string = axiosError.response.data as string
+                if(responseErrMessage.startsWith('email')){
+                    return { success: false, message: "", errors: {email:responseErrMessage} }
+                }else if(responseErrMessage.startsWith('username')){
+                    return { success: false, message: "", errors: {username:responseErrMessage} }
+                }else{
+                    return {success:false,message:responseErrMessage,errors:{}}
+                }
             } else if (axiosError.request) {
-                return { success: false, message: "Network Error! check your network and try again" }
+                return { success: false, message: "Network Error! check your network and try again", errors: {} }
             }
-            return { success: false, message: "something went wrong!" }
+            return { success: false, message: "Something went wrong!", errors: {} }
         }
-        return { success: false, message: "something went wrong!" }
+        return { success: false, message: "Something went wrong!", errors: {} }
     }
 }
--- src/components/auth/signup/SignupForm.tsx
+++ src/components/auth/signup/SignupForm.tsx
@@ -1,40 +1,50 @@
 'use client'
 
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Input from '../../ui/Input/Input'
 import { toast } from 'sonner';
 import FormButton from '../../ui/FormButton/FormButton';
 import { signupAction } from '@/src/app/actions';
 import { useFormState } from 'react-dom';
-import { ActionResponseState } from '@/src/types/authTypes';
+import { SignupActionState } from '@/src/types/authTypes';
 import { redirect } from 'next/navigation';
 
 const SignupForm = () => {
-    const initialState: ActionResponseState = { success: false, message: "" }
-    const [response,formAction] = useFormState(signupAction,initialState)
-    useEffect(()=>{
-        if(response.success){
+    const initialState: SignupActionState = {
+        success: false,
+        message: '',
+        errors:{
+            name:'',
+            username:'',
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    }
+    const [response, formAction] = useFormState(signupAction, initialState)
+    useEffect(() => {
+        if (response.success) {
             toast.success(response.message)
             redirect('/')
         }
-    },[response])
+    }, [response])
 
     return (
         <div className='border text-white bg-[#0D1117] w-[20rem] max-h-fit min-h-44 p-3 rounded-md border-[#30363D] flex-col flex justify-center'>
             <form action={formAction} className='flex flex-col items-left justify-center' >
                 <h1 className='text-center text-white w-100 font-medium text-2xl'>Singup to Pithub</h1>
                 <hr className='mt-4 border-[#30363D] ' />
-                <h4 className='text-left mt-3'>full name <span className='text-red-600 font-light text-sm '></span></h4>
+                <h4 className='text-left mt-3'>full name <br /> <span className='text-red-600 font-light text-sm '>{response.errors?.name && response.errors.name}</span></h4>
                 <Input name="name" type="text" />
-                <h4 className='text-left mt-3'>username <span className='text-red-600 font-light text-sm '></span></h4>
+                <h4 className='text-left mt-3'>username <br /> <span className='text-red-600 font-light text-sm '>{response.errors?.username && response.errors.username}</span></h4>
                 <Input name="username" type="text" />
-                <h4 className='text-left mt-3'>email <span className='text-red-600 font-light text-sm '></span></h4>
+                <h4 className='text-left mt-3'>email <br /> <span className='text-red-600 font-light text-sm '>{response.errors?.email && response.errors.email}</span></h4>
                 <Input name="email" type="email" />
-                <h4 className='text-left mt-3'>password <span className='text-red-600 font-light text-sm '></span></h4>
+                <h4 className='text-left mt-3'>password <br /> <span className='text-red-600 font-light text-sm '>{response.errors?.password && response.errors.password}</span></h4>
                 <Input name="password" type="password" />
-                <h4 className='text-left mt-3'>confirm password <span className='text-red-600 font-light text-sm '></span></h4>
-                <Input name="confirm" type="password" />
-                <FormButton/>
+                <h4 className='text-left mt-3'>confirm password <br /> <span className='text-red-600 font-light text-sm '>{response.errors?.con && response.errors.con}</span></h4>
+                <Input name="confirmPassword" type="password" />
+                <FormButton />
             </form>
         </div>
     )
