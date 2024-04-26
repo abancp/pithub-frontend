92a7a533af5e0865a9682e0f84b740f5e07b4803
Aban Muhammed C P <abancpchengani@gmail.com>
2024-04-26 08:07:52 +0530

    fix:for production

--- src/config/collections.ts
+++ src/config/collections.ts
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "https://pithub.onrender.com"
-export const SERVER_URL = "http://localhost:8000"
+export const SERVER_URL = "https://pithub.onrender.com"
+// export const SERVER_URL = "http://localhost:8000"
